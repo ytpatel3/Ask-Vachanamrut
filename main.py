5fa89d06c43fa164674c533690be6261992d4784
Yash Patel <ytpatel3@gmail.com>
2025-04-01 14:50:02 -0400

    initialize main.py and import relevant libraries

---
+++ main.py
@@ -0,0 +1,16 @@
+""" 
+Ask Vachanamrut 
+author: ytpatel3 
+""" 
+
+import re 
+# pip install rake-nltk 
+from rake_nltk import Rake 
+# pip install 'aisuite[openai]' 
+import aisuite as ai 
+
+def main(): 
+    print() 
+    
+if __name__ == "__main__": 
+    main()
